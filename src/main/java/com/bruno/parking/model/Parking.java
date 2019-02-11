6e5c1e0d8c811972174438355c6b2cf8d4afa18d
bhmyamashita <bhmyamashita@gmail.com>
2019-02-10 21:55:50 -0300

    preparando dev para heroku

--- src/main/java/com/bruno/parking/model/Parking.java
+++ src/main/java/com/bruno/parking/model/Parking.java
@@ -1,20 +1,20 @@
 package com.bruno.parking.model;
 
-import java.sql.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Parking {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy="increment")
 	private Long id;
 	
 	@NotEmpty
